71df895c5c4ab621d61cd2353393a5e9b05091aa
Patricio Pagani Palmbaum <Patriciosk8x@hotmail.com>
2017-11-09 12:42:16 -0300

    Weapon System

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon {
+
+    public static ObjectPool<Bullet> poolObject = new ObjectPool<Bullet>(1, new BulletFactory());
+
+    protected Transform _spawnPoint;
+    protected KeyCode _fireKey;
+    protected float _timer;
+    protected float _fireCoolDown;
+
+    public float fireCoolDown { get { return _fireCoolDown; } }
+
+    public Weapon(KeyCode kcFire, Transform spawnPoint, float fireCoolDown) {
+        _fireKey = kcFire;
+        _spawnPoint = spawnPoint;
+        _timer = 0;
+        this._fireCoolDown = fireCoolDown;
+    }
+
+    public Weapon() { }
+
+    public abstract void Shoot();
+    public abstract bool CheckTrigger();
+    public bool canShoot() { return _timer >= _fireCoolDown; }
+
+    public virtual void Update() {//Funcion a poner en el update del que poseea weapon (Enemy / Player)
+        _timer += Time.deltaTime;
+        if(CheckTrigger() && canShoot()) {
+            Shoot();
+            _timer = 0;
+        }
+    }
+
+    protected void PositionBullet(Bullet bullet) {
+        bullet.transform.position = _spawnPoint.position;
+        bullet.transform.rotation = _spawnPoint.rotation;
+    }
+}
+
