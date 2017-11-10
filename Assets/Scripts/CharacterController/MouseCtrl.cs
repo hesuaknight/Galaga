179ca756f17b47dd7205161c23e2d017e0c9d201
Patricio Pagani Palmbaum <Patriciosk8x@hotmail.com>
2017-11-10 12:32:51 -0300

    PlayerController

---
+++ Assets/Scripts/CharacterController/MouseCtrl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCtrl : PlayerController {
+    public MouseCtrl(Player ship) : base(ship) {
+        _axisMovement = "Mouse X";
+        _fireAttack = KeyCode.Mouse0;
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+    PlayerController controller;
+    Weapon weapon;
+
+	void Start () {
+        controller = new KeyboardCtrl(this);
+        weapon = new WeaponStandard(controller.fireKey, this.transform, 0.35f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller.Update();
+        weapon.Update();
+	}
+
+    public void Move(Vector3 dir) {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
