7c842e3df3c25d523e091cca4b045c2bf9561163
Christian Carapezza <carapezza.christian@gmail.com>
2017-11-24 17:52:51 -0300

    Estructuras: Formation, Level, Checkpoint, PowerUps  interface

---
+++ Assets/Scripts/Enemy/ICompositeFormation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICompositeFormation : IFormationComponent {
+    void AddShip();
+    void RemoveShip();
+    void AttackRandomComponents(int count);
+    void GetChild();
+}
---
+++ Assets/Scripts/PowerUps/PowerUpCheckpoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCheckpoint : IPowerUp {
+
+    public void OnTakePowerUP()
+    {
+        throw new NotImplementedException();
+    }
+}
