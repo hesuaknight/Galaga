fe2ca1699ea82735ffad79f4bb7a7f4dcd6af06a
lnanton <leandro.nanton@davinci.edu.ar>
2017-11-27 09:38:36 -0300

    Revert "Merge branch 'master' into GUI"
    This reverts commit f85d842af5be208cae9206e5f56832fa0b733a66, reversing
    changes made to 1d206a1c7445124b4baa980997f81c154cf98d1a.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,19 +7,17 @@ public class Player : MonoBehaviour {
     public float speed;
 
     PlayerController controller;
-    private Weapon _weapon;
-
-    public Weapon weapon { get { return _weapon; } set { _weapon = value; } }
+    Weapon weapon;
 
 	void Start () {
         controller = new KeyboardCtrl(this);
-        _weapon = new WeaponStandard(controller.fireKey, this.transform, 0.35f);
+        weapon = new WeaponStandard(controller.fireKey, this.transform, 0.35f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         controller.Update();
-        _weapon.Update();
+        weapon.Update();
 	}
 
     public void Move(Vector3 dir) {
